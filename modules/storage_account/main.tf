565adf259fa61d4a78a7cd6ecc0ec50e90f9f8d8
Arockia Nirmal Amala Doss <arockianirmal26@gmail.com>
2025-09-14 13:30:07 +0200

    fmt issues

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_storage_account" "this" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   # Enable Hierarchical Namespace for ADLS Gen2
-  is_hns_enabled           = true
-  min_tls_version          = "TLS1_2"
+  is_hns_enabled = true
+  min_tls_version = "TLS1_2"
   tags = {
     environment = "dev"
     workload    = "data-platform"
