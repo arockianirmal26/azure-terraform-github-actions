f4a4f41f6fd9c6a7c26b8e4fb8f20925368062d4
a.nirmal <arockia.nirmal@zippytec.de>
2025-09-14 15:37:00 +0200

    storage accounts

--- main.tf
+++ main.tf
@@ -22,7 +22,17 @@ provider "azurerm" {
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-aks" {
-  name     = var.resource_group_name
-  location = var.location
+# resource "azurerm_resource_group" "rg-aks" {
+#  name     = var.resource_group_name
+#  location = var.location
+# }
+
+
+# Call the storage account module
+module "adls_storage" {
+  source               = "./modules/storage_account"
+  storage_account_name = "synapsetest1298tf"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  containers           = ["raw-bronze", "refined-silver", "business-gold"]
 }
---
+++ modules/storage_account/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+  min_tls_version          = "TLS1_2"
+  #tags = {
+  #  environment = "dev"
+  #  workload    = "data-platform"
+  #}
+}
+
+resource "azurerm_storage_container" "this" {
+  for_each              = toset(var.containers)
+  name                  = each.key
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
+}
